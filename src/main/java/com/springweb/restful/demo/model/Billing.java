962aaaf1ac16af713a28fb9e8a43f3cd7215880f
Martin Champarini <martin.champarini.dev@gmail.com>
2018-11-19 00:19:53 -0300

    added model

---
+++ src/main/java/com/springweb/restful/demo/model/Billing.java
@@ -0,0 +1,31 @@
+
+package com.springweb.restful.demo.model;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Billing implements Serializable
+{
+
+    private Card card;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+    private final static long serialVersionUID = -2171696421877883987L;
+
+    public Card getCard() {
+        return card;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
---
+++ src/main/java/com/springweb/restful/demo/model/User.java
@@ -0,0 +1,32 @@
+
+package com.springweb.restful.demo.model;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class User implements Serializable
+{
+
+    private List<Datum> data = null;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+    private final static long serialVersionUID = -3270577580845452866L;
+
+    public List<Datum> getData() {
+        return data;
+    }
+
+    public void setData(List<Datum> data) {
+        this.data = data;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
