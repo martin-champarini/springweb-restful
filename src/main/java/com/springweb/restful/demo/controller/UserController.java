b096965d26063173f931157cc4c060c7bac41d5a
martin <martin.champarini.dev@gmail.com>
2018-11-22 22:49:06 -0300

    added swagger config and endpoint for test purpose

---
+++ src/main/java/com/springweb/restful/demo/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.springweb.restful.demo.controller;
+
+
+import com.springweb.restful.demo.model.Datum;
+import com.springweb.restful.demo.service.ApiServiceImp;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private ApiServiceImp apiServiceImp;
+
+
+    @GetMapping("getUsers")
+    public List<Datum> getAllUsers(@RequestParam @ApiParam("Limit for User list") Integer limit) {
+           return apiServiceImp.getUsers(limit);
+    }
+}
