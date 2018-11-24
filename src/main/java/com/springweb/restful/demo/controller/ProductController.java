4039863541c4be8ecdf4caeb70cffe3a09e43d94
martin <martin.champarini.dev@gmail.com>
2018-11-24 14:25:32 -0300

    Added Controller and Api info

--- src/main/java/com/springweb/restful/demo/controller/ProductController.java
+++ src/main/java/com/springweb/restful/demo/controller/ProductController.java
@@ -1,4 +1,16 @@
 package com.springweb.restful.demo.controller;
 
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Api(value="product-example", description="Operations pertaining to product in this RestFul Example")
 public class ProductController {
+
+    @GetMapping("/get-product")
+    public String getProduct (){
+        return "empty";
+    }
 }
--- src/main/java/com/springweb/restful/demo/controller/UserController.java
+++ src/main/java/com/springweb/restful/demo/controller/UserController.java
@@ -3,6 +3,8 @@ package com.springweb.restful.demo.controller;
 
 import com.springweb.restful.demo.model.user.Datum;
 import com.springweb.restful.demo.service.ApiServiceImp;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@Api(value="user-example", description="Operations pertaining to users in this RestFul Example")
 public class UserController {
 
     @Autowired
@@ -19,6 +22,7 @@ public class UserController {
 
 
     @GetMapping("getUsers")
+    @ApiOperation(value = "retrieve a list of users from apiary.io")
     public List<Datum> getAllUsers(@RequestParam @ApiParam("Limit for User list") Integer limit) {
            return apiServiceImp.getUsers(limit);
     }
